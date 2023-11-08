aeaea277e0723af0b466fbfc1dc52c8ae80b84a8
gerald <gombirog@gmail.com>
2023-11-08 13:49:33 +0200

    added win or lose conditions

--- src/main.rs
+++ src/main.rs
@@ -75,6 +75,10 @@ fn main() -> Result<(), Box<dyn Error>>{
             audio.play("move");
         }
 
+        if player.detect_hits(&mut invaders){
+            audio.play("explode")
+        }
+
         //draw & render
         let drawables : Vec<&dyn Drawable> = vec![&player, &invaders];
 
@@ -84,6 +88,17 @@ fn main() -> Result<(), Box<dyn Error>>{
 
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
+
+        //win or lose
+        if invaders.all_killed(){
+            audio.play("win");
+            break 'gameloop;
+        }
+
+        if invaders.reached_bottom(){
+            audio.play("lose");
+            break 'gameloop;
+        }
     }
 
     //Cleanup
